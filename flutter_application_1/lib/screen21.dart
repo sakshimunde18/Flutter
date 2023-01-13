ae7a17b23f62e6e23fe87b6d63b2e00c0c0e4d55
Sakshi Munde <92391500+sakshimunde18@users.noreply.github.com>
2023-01-13 22:43:37 +0530

    flutter basics

---
+++ flutter_application_1/lib/screen21.dart
@@ -0,0 +1,47 @@
+//Drawerclass
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class MyWidget21 extends StatefulWidget {
+  const MyWidget21({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return (Mystate());
+  }
+}
+
+class Mystate extends State<MyWidget21> {
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          drawer: Drawer(child: Builder(
+            builder: (context) {
+              return (ListView(
+                children: [
+                  DrawerHeader(
+                      decoration: BoxDecoration(color: Colors.blue),
+                      child: Text(
+                        'Header Information',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      )),
+                  ListTile(
+                    onTap: () {
+                      Fluttertoast.showToast(msg: 'option1');
+                      Navigator.pop(context);
+                    },
+                    title: Text('option2'),
+                    leading: Icon(Icons.exit_to_app),
+                  )
+                ],
+              ));
+            },
+          )),
+          appBar: AppBar(
+            title: Text('Drawer'),
+          )),
+    ));
+  }
+}
