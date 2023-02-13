8ebd22b3d6eb3fd182f4873b726568de7d610d06
sakshimunde18 <sakshimunde18@gmail.com>
2023-02-13 21:17:41 +0530

    Flutter

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Flutter-Basics
+# Flutter<br>
+Flutter is an open source framework by google.<br>
+For building beautiful natively compile multi-platform application from single code based.<br><br>
+Features Of Flutter:-<br>
+1. Flutter is Fast <br>
+   Flutter code is compile to machine code as well as javascript code.for fast performans as any platform.<br><br>
+2. Productive<br>
+   Flutter supports hot reload because of which we can build and check the application fast.<br><br>
+3. Flexible<br>
+   Flutter allows us to control every pixel on the screen which allows us to create customise UI.<br><br>
+4.  Multi-platform<br>
+   Flutter single project can be deployed on multiplatform like I-phone,android,web and desktop.<br><br> 
+5. Stable and reliable<br>
+   Flutter is maintain and supported by google and it has big community of developers.
  
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -4,6 +4,26 @@ import 'package:flutter_application_1/screen2.dart';
 import 'package:flutter_application_1/screen3.dart';
 import 'package:flutter_application_1/screen4.dart';
 import 'package:flutter_application_1/screen5.dart';
+import 'package:flutter_application_1/screen6.dart';
+import 'package:flutter_application_1/screen7.dart';
+import 'package:flutter_application_1/screen8.dart';
+import 'package:flutter_application_1/screen9.dart';
+import 'package:flutter_application_1/screen10.dart';
+import 'package:flutter_application_1/screen11.dart';
+import 'package:flutter_application_1/screen12.dart';
+import 'package:flutter_application_1/screen13.dart';
+import 'package:flutter_application_1/screen14.dart';
+import 'package:flutter_application_1/screen15.dart';
+import 'package:flutter_application_1/screen16.dart';
+import 'package:flutter_application_1/screen17.dart';
+import 'package:flutter_application_1/screen18.dart';
+import 'package:flutter_application_1/screen19.dart';
+import 'package:flutter_application_1/screen20.dart';
+import 'package:flutter_application_1/screen21.dart';
+import 'package:flutter_application_1/screen22.dart';
+import 'package:flutter_application_1/screen23.dart';
+import 'package:flutter_application_1/screen24.dart';
+import 'package:flutter_application_1/screen25.dart';
 void main() {
   runApp(MyWidget5 ());
 }
